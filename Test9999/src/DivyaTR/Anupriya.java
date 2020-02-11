8dba7c7b2a949c0226a1ca11a84fc238fdc7ac7c
srinivas.godari <srinivas.godari@HDC2-L-9227QQ2.dir.svc.accenture.com>
2020-02-11 22:26:20 +0530

    changes 2.0

--- Test9999/src/DivyaTR/Anupriya.java
+++ Test9999/src/DivyaTR/Anupriya.java
@@ -8,7 +8,7 @@ WebDriver driver;
 
 public void launching()
 {
-
+System.out.println("hi mr srinivas");
 System.setProperty("webdriver.chrome.driver","C:\\Drivers\\Drivers\\chromedriver.exe");
 driver = new ChromeDriver();
 driver.get("http://10.243.204.12:8083/TestMeApp");
