f391880bd5159516d9393b5ec12b85e253e63678
srinivas.godari <srinivas.godari@HDC2-L-9227QQ2.dir.svc.accenture.com>
2020-02-11 22:20:32 +0530

    First commit

---
+++ Test9999/src/DivyaTR/Anupriya.java
@@ -0,0 +1,16 @@
+package DivyaTR;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Anupriya {
+WebDriver driver;
+
+public void launching()
+{
+
+System.setProperty("webdriver.chrome.driver","C:\\Drivers\\Drivers\\chromedriver.exe");
+driver = new ChromeDriver();
+driver.get("http://10.243.204.12:8083/TestMeApp");
+}
+}
